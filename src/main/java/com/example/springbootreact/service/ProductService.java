a09a84cb0c6a57bb48f73ccbe1166e434b61f1e5
misha141 <mishasingh141@gmail.com>
2024-11-20 22:50:56 -0700

    fixed the build issue

--- src/main/java/com/example/springbootreact/service/ProductService.java
+++ src/main/java/com/example/springbootreact/service/ProductService.java
@@ -1,10 +1,13 @@
 package com.example.springbootreact.service;
 
 import com.example.springbootreact.model.Product;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 import java.util.Optional;
 
+@Configuration
 public interface ProductService {
     List<Product> getAllProduct();
     Optional<Product> getProductById(Long id);
--- src/main/java/com/example/springbootreact/service/ProductServiceImpl.java
+++ src/main/java/com/example/springbootreact/service/ProductServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.springbootreact.service;
 import com.example.springbootreact.model.Product;
 import com.example.springbootreact.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ProductServiceImpl implements ProductService{
 
     @Autowired
